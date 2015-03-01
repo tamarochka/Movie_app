56aec77a26df1c8680e1aae1d6cc23bfcd2ee6fd
tamarochka <tamara.zenkevich@gmail.com>
2015-02-28 21:37:52 -0500

    created database and test for movies uploading

---
+++ spec/features/create_book_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+feature 'user creates movies', %Q{
+  As a user
+  I want to upload movies file
+  So i can save and view them
+  } do
+    scenario 'valid data' do
+
+      visit movies_path
+
+      attach_file "File", File.join(Rails.root, "spec/support/files/example.csv")
+
+      click_button 'Import'
+
+      expect(page).to have_content("Movies successfully uploaded")
+      expect(Movie.count).to eq(10)
+    end
+
+    scenario 'with no file' do
+      visit movies_path
+
+      click_button 'Import'
+
+      expect(page).to have_content('Please attach file')
+      expect(Movie.count).to eq(0)
+    end
+  end
